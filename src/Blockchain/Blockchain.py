2bda5487146422041c78ff44f45de75e9b37539c
Ilias4652 <92111008+Ilias4652@users.noreply.github.com>
2026-06-30 16:08:23 +0000

    Basic blockchain implementation with proof of work and node registration.

--- src/Blockchain/Blockchain.py
+++ src/Blockchain/Blockchain.py
@@ -1,13 +1,17 @@
 import hashlib
 import json
 from time import time
+from urllib.parse import urlparse
+
+import requests
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
         if not self.chain:  # creates genesis block
             self.new_block(previous_hash=1, proof=100)
-
+        self.nodes = set()
+        
     def new_block(self, proof, previous_hash=None):
         #initiates a new block to the blockchain
         block ={
@@ -39,7 +43,13 @@ class Blockchain(object):
         })
         return self.last_block['index']+1
 
-
+    def register_node(self,address):
+        """
+        Add a new node to the list of nodes
+        :param address: <str> Address of node. Eg. 'http://
+        """
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
     @staticmethod
     def hash(block):
         #hashes a block by creating a a SHA-256 hash of a Block
@@ -68,7 +78,7 @@ class Blockchain(object):
 
         return proof
     
-
+    @staticmethod
     def valid_proof(last_proof, proof):
         """
         Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeroes?
@@ -80,3 +90,58 @@ class Blockchain(object):
         guess = f'{last_proof}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
+    
+    def valid_chain(self,chain):
+        """
+        Determine if a given blockchain is valid
+        :param chain: <list> A blockchain
+        :return: <bool> True if valid, False if not
+        """
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print("\n-----------\n")
+            # Check that the hash of the block is correct
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+
+            # Check that the Proof of Work is correct
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
+
+            last_block = block
+            current_index += 1
+
+        return True
+    
+
+    def resolve_conflicts(self):
+        """
+        This is our consensus algorithm, it resolves conflicts
+        by replacing our chain with the longest one in the network.
+        :return: <bool> True if our chain was replaced, False if not
+        """
+        neighbours = self.nodes
+        new_chain = None
+
+        max_length = len(self.chain)
+        for node in neighbours:
+            response = requests.get(f'http://{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
